2d04ecd67edf7cbc99e5a2da692be813acec1670
Gilles Pilon <gillespilon13@gmail.com>
2021-05-19 14:01:30 -0500

    Extract text from a PDF file.

---
+++ extract_text.py
@@ -0,0 +1,97 @@
+#! /usr/bin/env python3
+'''
+Extract text from a PDF file.
+'''
+
+from pathlib import Path
+from io import StringIO
+
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+import datasense as ds
+
+
+def main():
+    title_file_name_in = 'Name of pdf file to read?'
+    title_file_name_out = 'Name of txt file to save as?'
+    file_types_in = [('pdf files', '.pdf .PDF')]
+    file_types_out = [('txt files', '.txt .TXT')]
+    path_to_file_in = ds.ask_open_file_name_path(
+        title=title_file_name_in,
+        filetypes=file_types_in
+    )
+    path_to_file_out = ds.ask_save_as_file_name_path(
+        title=title_file_name_out,
+        filetypes=file_types_out
+    )
+    string_with_lines = pdf_to_text(path=path_to_file_in)
+    tidy = tidy_string(s=string_with_lines)
+    save_to_file(
+        path=path_to_file_out,
+        s=tidy
+    )
+
+
+def save_to_file(
+    path: Path,
+    s: str
+):
+    '''
+    Save a string to a file.
+    '''
+    file_to_save = open(
+        file=path,
+        mode='w'
+    )
+    file_to_save.write(s)
+
+
+def tidy_string(s: str) -> str:
+    '''
+    Strip empty lines, leading spaces, trailing spaces from a string.
+    '''
+    lines = s.split('\n')
+    non_empty_lines = [line for line in lines if line.strip() != '']
+    tidy = ''
+    for line in non_empty_lines:
+        tidy += line.strip() + '\n'
+    return tidy
+
+
+def pdf_to_text(path: Path) -> str:
+    '''
+    Extract all text from PDF file into a string.
+    '''
+    manager = PDFResourceManager()
+    retstr = StringIO()
+    layout = LAParams(all_texts=True)
+    device = TextConverter(
+        manager,
+        retstr,
+        laparams=layout
+    )
+    filepath = open(
+        file=path,
+        mode='rb'
+    )
+    interpreter = PDFPageInterpreter(
+        manager,
+        device
+    )
+    for page in PDFPage.get_pages(
+        filepath,
+        check_extractable=True
+    ):
+        interpreter.process_page(page)
+    text = retstr.getvalue()
+    filepath.close()
+    device.close()
+    retstr.close()
+    return text
+
+
+if __name__ == "__main__":
+    main()
